5093ba37c5cbf232295745e644a0a68ac696e41f
Han Liu <hanliu082@gmail.com>
2019-04-21 14:29:01 -0400

    Add layout.hbs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
