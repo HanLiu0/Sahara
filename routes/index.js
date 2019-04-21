011a1bca6f1c9630c0dfa901b0af34a6b0f925d2
Han Liu <hanliu082@gmail.com>
2019-04-21 15:44:17 -0400

    Add database file. Add sample.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var router = express.Router();
-
+var router = require('express').Router();
+var sql = require('../database/mysqlLib');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  sql.getItems(function(err, results) {
+    console.log(results);
+    res.render('index', { products: results });
+  });
+
 });
 
 module.exports = router;
