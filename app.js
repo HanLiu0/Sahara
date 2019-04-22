6ec1ce769e49afe06afb45b46d02020dfa1a8baa
mandykwokk <mandy25093@gmail.com>
2019-04-22 00:27:12 -0400

    login

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sellersRouter = require('./routes/seller');
 var shoppingCartRouter = require('./routes/shopping_cart');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -26,7 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/seller', sellersRouter);
-app.use('/shoppingCart',shoppingCartRouter);
+app.use('/shoppingCart', shoppingCartRouter);
+app.use('/login', loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
