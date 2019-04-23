8ba0d0004361af5420da6d499610986c284b5714
Han Liu <hanliu082@gmail.com>
2019-04-23 13:18:06 -0400

    Fix javascript problem. Add account overview subpages.

--- routes/account_overview.js
+++ routes/account_overview.js
@@ -17,4 +17,20 @@ router.post('/edit_account_information', function(req, res, next) {
     sql.editAccountInformation(req.body);
     res.redirect('/');
 });
+
+router.get('/payment_options', function(req, res, next) {
+    res.render('payment_options', { title : "Payment Options"});
+});
+
+router.get('/listed_items', function(req, res, next) {
+    res.render('listed_items', { title : "Listed Items"});
+});
+
+router.get('/add_item', function(req, res, next) {
+    res.render('add_item', { title : "Add Item"});
+});
+
+router.get('/edit_seller_information', function(req, res, next) {
+    res.render('edit_seller_information', { title : "Edit Seller Information"});
+});
 module.exports = router;
