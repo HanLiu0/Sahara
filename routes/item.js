3ead7c7fa29f88324573010f31fe569d4ed4ed64
mandykwokk <mandy25093@gmail.com>
2019-04-29 14:29:08 -0400

    Merge branch 'master' of C:\Users\mandy\IdeaProjects\Sahara with conflicts.

--- routes/item.js
+++ routes/item.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 var sql = require('../database/mysqlLib');
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', isLoggedIn, function (req, res, next) {
     sql.getItemByID(req.params.id, function (err, preresult, results) {
         res.render('item', {title: "Item Page",item: results[0],no_review: preresult[0]===undefined,list: results,sum:0});
     });
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/signin');
+}
