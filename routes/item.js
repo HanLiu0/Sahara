9047290cdfa9143727439c1ce47c0e4781b4188c
mandykwokk <mandy25093@gmail.com>
2019-05-02 19:04:42 -0400

    Merge branch 'master' of C:\Users\mandy\IdeaProjects\Sahara with conflicts.

--- routes/item.js
+++ routes/item.js
@@ -14,10 +14,10 @@ router.post('/item_review/:id', function(req, res, next){
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     sql.editItemReview(req.params.id, req.user, req.body.rating,req.body.detail,date);
-    //res.redirect('/item/'+req.params.id); //refresh a page
-    sql.getItemByID(req.params.id, function (err, preresult, results, result_with_rating) {
-        res.render('item', {title: "Item Page",item: results[0],no_review: preresult[0]===undefined,list: results,sum:0, rate: result_with_rating[0]});
-    });
+    res.redirect('/item/'+req.params.id); //refresh a page
+//    sql.getItemByID(req.params.id, function (err, preresult, results, result_with_rating) {
+  //      res.render('item', {title: "Item Page",item: results[0],no_review: preresult[0]===undefined,list: results,sum:0, rate: result_with_rating[0]});
+   // });
 });
 
 router.post('/add_to_cart/:id', function(req, res, next){
