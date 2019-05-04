33c581e1d3778adfd61c7718f4b5eb20e1b3a3c2
Han Liu <hanliu082@gmail.com>
2019-05-03 22:18:31 -0400

    Add stylesheet for signin and signup page.

--- routes/user/signin.js
+++ routes/user/signin.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var sql = require('../../database/mysqlLib');
 var passport = require('passport/lib');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     var messages = req.flash('error');
-    res.render('user/signin', { title : "Sahara.com: Sign In", messages: messages, errors: messages.length > 0 });
+    res.render('user/signin', {title: "Sahara.com: Sign In", messages: messages[0], errors: messages.length > 0});
 });
 
 router.post('/', passport.authenticate('local.signin', {
     failureRedirect: 'signin',
     failureFlash: true
 }), function (req, res, next) {
-        res.redirect('/');
+    res.redirect('/');
 });
 
 module.exports = router;
